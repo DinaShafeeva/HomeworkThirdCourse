95c63f1a3a86b8ec35a201fbf2bb6794a831501a
DinaShafeeva <49372442+DinaShafeeva@users.noreply.github.com>
2020-10-15 18:26:03 +0300

    First version

--- app/src/main/java/com/example/homeworkthirdcourse/MainActivity.kt
+++ app/src/main/java/com/example/homeworkthirdcourse/MainActivity.kt
@@ -1,11 +1,31 @@
 package com.example.homeworkthirdcourse
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.view.animation.Animation
+import android.view.animation.AnimationUtils
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val animation = AnimationUtils.loadAnimation(this, R.anim.alpha)
+        btn_2.startAnimation(animation)
+    }
+
+    fun onAnimationStart(animation: Animation?) {
+        btn_2.visibility = View.VISIBLE
+    }
+
+    fun onAnimationEnd(animation: Animation?) {
+        btn_2.visibility = View.INVISIBLE
+    }
+
+    fun onAnimationRepeat(animation: Animation?) {
+        btn_2.visibility = View.VISIBLE
     }
 }
